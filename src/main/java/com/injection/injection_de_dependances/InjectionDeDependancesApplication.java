cd4931bbce5cb7a647d0b0f9a013f7529b48ae80
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-07 15:10:54 +0100

    first commit

---
+++ src/main/java/com/injection/injection_de_dependances/InjectionDeDependancesApplication.java
@@ -0,0 +1,13 @@
+package com.injection.injection_de_dependances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionDeDependancesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjectionDeDependancesApplication.class, args);
+    }
+
+}
