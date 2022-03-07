b14e6de5852b29273da624ea8a90f69c8ef379ab
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-03-07 15:11:25 +0100

    add class dao user

---
+++ src/main/java/com/injection/injection_de_dependances/UserDAO.java
@@ -0,0 +1,8 @@
+package com.injection.injection_de_dependances;
+
+public class UserDAO {
+    public String[] getUsers(){
+        String[] users= {"mohssinr","elattari"};
+        return users;
+    }
+}
